ed6a73b53656cc9429fef6420c0799b80a41d2ac
bharath nidumolu <nidumolubharath230@gmail.com>
2026-04-05 03:23:23 -0500

    Delete ai_storyboard directory

--- ai_storyboard/providers/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Optional
-
-
-class LLMProvider(ABC):
-    @abstractmethod
-    def complete(self, user: str, system: Optional[str] = None) -> str:  # pragma: no cover
-        raise NotImplementedError
